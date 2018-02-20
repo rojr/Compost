1e824b09b96f1fcbd583d8b594b2e02bd946f392
robmadeyou <robmadeyou@gmail.com>
2018-02-20 16:17:05 +0000

    Adding few nice icons :)

--- src/icons/CompostIcons.java
+++ src/icons/CompostIcons.java
@@ -5,4 +5,6 @@ import javax.swing.*;
 
 public interface CompostIcons {
     Icon RHUBARB_ICON = IconLoader.getIcon("/icons/rhubarbicon.png");
+    Icon LEAF_ICON = IconLoader.getIcon("/icons/leaf16.png");
+    Icon MODULE_ICON = IconLoader.getIcon("/icons/cherryPick.png");
 }
